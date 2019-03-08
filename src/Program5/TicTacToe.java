264d04da6eb14417d6e83c045d9006ea872c9445
mbinelli22 <mbinelli1996@gmail.com>
2019-03-08 12:01:15 -0600

    Added the checkWinner methods for players 1 & 2

--- src/Program5/TicTacToe.java
+++ src/Program5/TicTacToe.java
@@ -13,7 +13,21 @@ public class TicTacToe {
 		System.out.println("Player two enter your name: ");
 		player2 = stdIn.nextLine();
 	}
-	
+	  public static  String referenceBoard()
+	    {
+		  System.out.print("Use this as your referance board throughout the game:");
+	        System.out.println(  "\n\n\t\t    |    |  ");
+	        System.out.println(  " \t\t  1 |  2 |  3 " );
+	        System.out.println(  " \t\t ___|____|___ " );
+	        System.out.println(  "\t\t    |    |    " );
+	        System.out.println(  " \t\t  4 | 5  |  6 " );
+	        System.out.println(  " \t\t ___|____|___ " );
+	        System.out.println(  "\t\t    |    |    ");
+	        System.out.println(  " \t\t  7 |  8 | 9  " );
+	        System.out.println(  " \t\t    |    |   " );
+	        System.out.println(  "\n\n" );
+	        return "currentBoard";
+	    }
 	  public static  String currentBoard()
 	    {
 	        System.out.println(  "\n\n\t\t" + gridLocation [1] + "   | " +gridLocation [2]+ "  | " +gridLocation [3]);
@@ -28,16 +42,54 @@ public class TicTacToe {
 	        System.out.println(  "\n\n" );
 	        return "currentBoard";
 	    }
-	
-	
-	
-	
-	
-	
+	  public static char checkWinnerForPlayer1() {
+		  char winner = '';
+		  if(gridLocation[1] == 'X' && gridLocation[2] == 'X' && gridLocation[3] == 'X')
+			  winner = 'X';
+	        if (posn[4] == 'X' && posn[5] == 'X' && posn[6] == 'X')
+	        	winner = 'X';
+	        if (posn[7] == 'X' && posn[8] == 'X' && posn[9] == 'X') 
+	        	winner = 'X';
+	        if (posn[1] == 'X' && posn[4] == 'X' && posn[7] == 'X') 
+	        	winner = 'X';
+	        if (posn[2] == 'X' && posn[5] == 'X' && posn[8] == 'X') 
+	        	winner = 'X';
+	        if (posn[3] == 'X' && posn[6] == 'X' && posn[9] == 'X') 
+	        	winner = 'X';
+	        if (posn[1] == 'X' && posn[5] == 'X' && posn[9] == 'X') 
+	        	winner = 'X';
+	        if (posn[3] == 'X' && posn[5] == 'X' && posn[7] == 'X') 
+	        	winner = 'X';
+	        if(winner == "X") 
+	        	System.out.println(player1 + " Wins!!");
+	  }
+	  public static char checkWinnerForPlayer2() {
+		  char winner = '';
+		  if(gridLocation[1] == 'O' && gridLocation[2] == 'O' && gridLocation[3] == 'O')
+			  winner = 'O';
+	        if (posn[4] == 'O' && posn[5] == 'O' && posn[6] == 'O')
+	        	winner = 'O';
+	        if (posn[7] == 'O' && posn[8] == 'O' && posn[9] == 'O') 
+	        	winner = 'O';
+	        if (posn[1] == 'O' && posn[4] == 'O' && posn[7] == 'O') 
+	        	winner = 'O';
+	        if (posn[2] == 'O' && posn[5] == 'O' && posn[8] == 'O') 
+	        	winner = 'O';
+	        if (posn[3] == 'O' && posn[6] == 'O' && posn[9] == 'O') 
+	        	winner = 'O';
+	        if (posn[1] == 'O' && posn[5] == 'O' && posn[9] == 'O') 
+	        	winner = 'O';
+	        if (posn[3] == 'O' && posn[5] == 'O' && posn[7] == 'O') 
+	        	winner = 'O';
+	        if(winner == "O") 
+	        	System.out.println(player2 + " Wins!!");
+	  }
 	
 	
 	public static void main(String[] args) {
+		TicTacToe game = new TicTacToe();
 		System.out.println("Tic-Tac-Toe");
+		referenceBoard();
 		getPlayerNames();
 		currentBoard();
 	}
